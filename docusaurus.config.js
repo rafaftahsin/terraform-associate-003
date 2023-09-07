d629b0d382e0a483b9d104b6b8343215c9dc50c4
Rafaf Tahsin <rafaf.tahsin@infolytx.com>
2023-09-07 07:49:35 +0600

    re organized repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,11 +75,11 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Notes',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/rafaftahsin/terraform-associate-003',
             label: 'GitHub',
             position: 'right',
           },
